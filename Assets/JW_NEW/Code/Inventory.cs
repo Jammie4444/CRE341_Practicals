865a786cd73045b0e22b453e6dae9bdb219388e5
Jamie Wright <wrightjamie360@icloud.com>
2025-03-06 20:57:34 +0000

    UI for Gems

--- Assets/JW_NEW/Code/Inventory.cs
+++ Assets/JW_NEW/Code/Inventory.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    public TMP_Text diamondText;
+    public TMP_Text rubyText;
+    public TMP_Text emeraldText;
+
+    public int diamondGems = 0;
+    public int rubyGems = 0;
+    public int emeraldGems = 0;
+
     public static Inventory instance;  
     public List<Item> items = new List<Item>();
-
-    [SerializeField] TextMeshProUGUI gemText;
 
     void Awake()
     {
@@ -16,6 +23,13 @@ public class Inventory : MonoBehaviour
             Destroy(gameObject);
         else 
             instance = this;
+    }
+
+    void Update()
+    {
+        diamondText.text = "Diamonds: " + diamondGems + "/3";
+        rubyText.text = "Rubys: " + rubyGems + "/3";
+        emeraldText.text = "Emeralds: " + emeraldGems + "/3";
     }
 
     public void AddItem(Item itemToAdd)
@@ -26,6 +40,21 @@ public class Inventory : MonoBehaviour
         {
             if (item.name == itemToAdd.name)
             {
+                if (itemToAdd.name == "Diamond")
+                {
+                    Debug.Log("Diamond Collected");
+                    diamondGems += 1;
+                }
+                if (itemToAdd.name == "Ruby")
+                {
+                    Debug.Log("Ruby Collected");
+                    rubyGems++; 
+                }
+                if (itemToAdd.name == "Emerald")
+                {
+                    Debug.Log("Emerald Collected");
+                    emeraldGems++;
+                }
                 item.count += itemToAdd.count;
                 itemExists = true;  
                 break;
@@ -35,7 +64,7 @@ public class Inventory : MonoBehaviour
         {
             items.Add(itemToAdd);
         }
-        Debug.Log(itemToAdd.count + " " +  itemToAdd.name + "added to inventory");
+        Debug.Log(itemToAdd.count + " " +  itemToAdd.name + " added to inventory");
     }
 
     //public void RemoveItem(Item itemToRemove)
